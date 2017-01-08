e88c8822d5039200412256653ea60f895193bfa8
silentworker <silentworker@yandex.ua>
2017-01-08 22:04:17 +0200

    First

---
+++ Assets/Script/PlayGround/ConstantSpeedMoving.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+    public class ConstantSpeedMoving : MonoBehaviour
+    {
+        public float Speed;
+        public float RepeatPartSize;
+
+        private bool _motion;
+        private RectTransform _rectTransform;
+        private float _startXPosition;
+
+        void Start()
+        {
+            _rectTransform = gameObject.GetComponent<RectTransform>();
+            _startXPosition = _rectTransform.anchoredPosition.x;
+            _motion = true;
+        }
+
+        void Update()
+        {
+            if (!_motion) return;
+
+            _rectTransform.Translate(Vector2.left * Speed * Time.deltaTime);
+
+            if (_rectTransform.anchoredPosition.x < _startXPosition - RepeatPartSize)
+            {
+                _rectTransform.anchoredPosition = new Vector3(_startXPosition, _rectTransform.anchoredPosition.y);
+            }
+        }
+
+        public void Move()
+        {
+            _motion = true;
+        }
+
+        public void Stop()
+        {
+            _motion = false;
+        }
+    }
+}
---
+++ Assets/Script/PlayGround/PlayGroundController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+    public class PlayGround : MonoBehaviour
+    {
+        public ConstantSpeedMoving Clouds;
+        public ConstantSpeedMoving Water;
+        public ConstantSpeedMoving Fog;
+        public ConstantSpeedMoving Leafs;
+        public ConstantSpeedMoving Liana;
+
+        public void Move()
+        {
+            Clouds.Move();
+            Water.Move();
+            Fog.Move();
+            Leafs.Move();
+            Liana.Move();
+        }
+
+        public void Stop()
+        {
+            Clouds.Stop();
+            Water.Stop();
+            Fog.Stop();
+            Leafs.Stop();
+            Liana.Stop();
+        }
+    }
+}
